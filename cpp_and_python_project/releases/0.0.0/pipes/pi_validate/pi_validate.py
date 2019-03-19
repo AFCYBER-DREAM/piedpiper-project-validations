d2f0382a21c32e01584ed04919b9ab28a43f0d4e
Nick Wilburn <senior.crepe@gmail.com>
2019-03-19 12:39:35 -0500

    Cause failure

--- cpp_and_python_project/releases/0.0.0/pipes/pi_validate/pi_validate.py
+++ cpp_and_python_project/releases/0.0.0/pipes/pi_validate/pi_validate.py
@@ -19,7 +19,7 @@ class ValidatePipeVars(Schema):
 
     @validates('run_pipe')
     def validate_run_pipe(self, value):
-        defined_value = True
+        defined_value = False
         if value != defined_value:
             raise ValidationError('Validate Pipe must be enabled')
 
