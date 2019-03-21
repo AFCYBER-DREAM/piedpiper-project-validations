95ade1ee0a66a8546c11f58981799c958af92992
Nick Wilburn <senior.crepe@gmail.com>
2019-03-21 08:42:23 -0500

    yes

--- cpp_and_python_project/releases/0.0.1/pipes/pi_gitlab_ci/pi_gitlab_ci.py
+++ cpp_and_python_project/releases/0.0.1/pipes/pi_gitlab_ci/pi_gitlab_ci.py
@@ -11,7 +11,6 @@ class Stages(Schema):
     def validate_stages(self, value):
         required_stages = [
             'validate',
-            'add',
             'lint'
         ]
         errors = []
