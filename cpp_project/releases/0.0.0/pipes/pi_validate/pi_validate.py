c005b82fb3badae87720ad3df5d42fcd69f2d33d
Nick Wilburn <senior.crepe@gmail.com>
2019-03-21 09:00:14 -0500

    yes

--- cpp_project/releases/0.0.0/pipes/pi_validate/pi_validate.py
+++ cpp_project/releases/0.0.0/pipes/pi_validate/pi_validate.py
@@ -19,7 +19,7 @@ class ValidatePipeVars(Schema):
 
     @validates('run_pipe')
     def validate_run_pipe(self, value):
-        defined_value = False
+        defined_value = True
         pipe_boolean = ('disabled', 'enabled')[defined_value]
         if value != defined_value:
             raise ValidationError(f'Validate Pipe must be {pipe_boolean}')
